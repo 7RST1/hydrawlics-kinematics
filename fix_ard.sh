cd29d61238c9defe7e03d19fe4798d3da14368d7
Syver Stensholt <stonegrove@duck.com>
2025-10-05 23:27:15 +0200

    started implementating coord transfer protocol

---
+++ fix_ard.sh
@@ -0,0 +1,1 @@
+arduino-cli upload -p /dev/ttyACM0 -b arduino:avr:mega -v --verbose --discovery-timeout 30s
--- hydrawlics_kinematics.ino
+++ hydrawlics_kinematics.ino
@@ -1,9 +1,56 @@
-void setup() {
-  // put your setup code here, to run once:
+//String TEST_LINE = String("");
 
+String TEST_LINE
+{R"(G0 Z1
+G0 X30 Y50
+G0 Z0
+G1 X35 Y45
+G1 X40 Y42
+G1 X45 Y40
+G1 X50 Y40
+G1 X55 Y40
+G1 X60 Y42
+G1 X65 Y45
+G1 X70 Y50
+G0 Z1
+G0 X45 Y60
+G0 Z0
+G1 X45 Y65
+G0 Z1
+G0 X55 Y60
+G0 Z0
+G1 X55 Y65
+G0 Z1
+)"};
+
+// NOTE: The board resets automatically when a Serial connection is estabished..
+// Therefore the whole 
+
+void setup(){
+  Serial.begin(115200);
+  Serial.println("Connected");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+  // some start up time
+  delay(2000);
+
+  if(Serial.available()) {
+    //String readed = Seial.readStringUntil('\n');
+    String readed = Serial.readString();
+    readed.trim();
+    processLine(readed);
+
+    delay(200);
+    //Serial.println("OK");
+  }
+}
+
+void processLine(String &line) {
+  uint8_t checksum = 0;
+  for(int i = 0; i < line.length(); i++) {
+      checksum ^= line[i];  // XOR all bytes
+  }
+  Serial.println(String("OK ") + checksum);
 }
